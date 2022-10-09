7fa487b9bf903cd9367afc8fc376efc88a523ba7
Justin Chu <justinchuby@users.noreply.github.com>
2022-10-09 09:46:09 -0700

    Use pipenv

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
-pip install -r requirements.txt
+pipenv install --skip-lock
