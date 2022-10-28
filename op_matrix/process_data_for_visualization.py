d4877b31b7d535debdf1965e20e89fa00ce87c01
Justin Chu <justinchuby@users.noreply.github.com>
2022-10-27 19:49:08 -0700

    process

--- op_matrix/process_data_for_visualization.py
+++ op_matrix/process_data_for_visualization.py
@@ -2,9 +2,12 @@ from collections import defaultdict
 import json
 import os
 import argparse
+import random
 
 
-def process_data_for_visualization(data: dict) -> list[dict]:
+def process_data_for_visualization(
+    data: dict, sample_exceptions: int | None = None
+) -> list[dict]:
     results = defaultdict(dict)
     for test_result in data["test_results"]:
         operator = test_result["operator"]
@@ -17,27 +20,40 @@ def process_data_for_visualization(data: dict) -> list[dict]:
             "correct_count": correct,
             "total_count": total,
             "aten_name": aten_name,
-            "exceptions": exceptions,
+            "exceptions": random.sample(exceptions, sample_exceptions)
+            if sample_exceptions and len(exceptions) >= sample_exceptions
+            else exceptions,
         }
 
     return [{"operator": operator, **results} for operator, results in results.items()]
 
 
 def main(args):
-    with open(args.input, "r") as f:
-        data = json.load(f)
-
-    results = process_data_for_visualization(data)
+    results = []
+    for file in sorted(os.listdir(args.input_dir), reverse=True):
+        if file.endswith(".json"):
+            with open(os.path.join(args.input_dir, file), "r") as f:
+                data = json.load(f)
+                processed_data = process_data_for_visualization(data, args.sample_exceptions)
+                result = {
+                    "file": file,
+                    "torch_version": data["torch_version"],
+                    "onnx_version": data.get("onnx_version"),
+                    "opset": data["test_results"][0]["opset"],
+                    "test_results": processed_data,
+                }
+                results.append(result)
 
     output_dir = os.path.dirname(args.output)
     os.makedirs(output_dir, exist_ok=True)
     with open(args.output, "w") as f:
-        json.dump(results, f)
+        json.dump(results, f, indent=2)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, required=True)
+    parser.add_argument("--input_dir", type=str, required=True)
     parser.add_argument("--output", type=str, required=True)
+    parser.add_argument("--sample_exceptions", type=int, default=2)
     args = parser.parse_args()
     main(args)
