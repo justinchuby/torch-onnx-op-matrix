41cadd69482a0b5e65829e9b2478ba2000774367
Justin Chu <justinchuby@users.noreply.github.com>
2026-01-01 15:27:51 -0800

    Update install.sh to modify ONNX package installations (#49)
    Removed onnx-weekly installation and adjusted onnxscript installation.

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 
 pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 pip install -r requirements.txt
-pip install onnx-weekly
-pip install --no-deps onnxscript
+pip install onnxscript
